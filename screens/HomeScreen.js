26949cbb3d00a3352890625b69062c2ebc52d462
Ferdy <93808738+verdia20@users.noreply.github.com>
2023-05-14 22:19:25 +0700

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,224 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Alert, Pressable, Image, TextInput, ScrollView } from "react-native";
+import * as Location from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import Carousel from "../components/Carousel";
+import Services from "../components/Services";
+import DressItem from "../components/DressItem";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../ProductReducer";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  const [items, setItems] = useState([]);
+  const total = cart.map((item) => item.quantity * item.price).reduce((curr, prev) => curr + prev, 0);
+  const navigation = useNavigation();
+  console.log(cart);
+  const [displayCurrentAddress, setdisplayCurrentAddress] = useState("Sedang memuat lokasi Anda");
+  const [locationServicesEnabled, setlocationServicesEnabled] = useState(false);
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+    if (!enabled) {
+      Alert.alert(
+        "Layanan lokasi tidak diaktifkan",
+        "Harap aktifkan layanan lokasi",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      setlocationServicesEnabled(enabled);
+    }
+  };
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Izin ditolak",
+        "izinkan aplikasi untuk menggunakan layanan lokasi",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    }
+
+    const { coords } = await Location.getCurrentPositionAsync();
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      for (let item of response) {
+        let address = `${item.name} ${item.city} ${item.postalCode}`;
+        setdisplayCurrentAddress(address);
+      }
+    }
+  };
+
+  const product = useSelector((state) => state.product.product);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (product.length > 0) return;
+
+    const fetchProducts = () => {
+      services.map((service) => dispatch(getProducts(service)));
+    };
+    fetchProducts();
+  });
+  const services = [
+    {
+      id: "0",
+      image: "https://cdn-icons-png.flaticon.com/128/4643/4643574.png",
+      name: "Kemeja",
+      quantity: 0,
+      price: 7500,
+    },
+    {
+      id: "11",
+      image: "https://cdn-icons-png.flaticon.com/128/892/892458.png",
+      name: "Kaos",
+      quantity: 0,
+      price: 5000,
+    },
+    {
+      id: "12",
+      image: "https://cdn-icons-png.flaticon.com/128/9609/9609161.png",
+      name: "Gaun",
+      quantity: 0,
+      price: 50000,
+    },
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/599/599388.png",
+      name: "Jeans",
+      quantity: 0,
+      price: 15000,
+    },
+    {
+      id: "14",
+      image: "https://cdn-icons-png.flaticon.com/128/9431/9431166.png",
+      name: "Sweater",
+      quantity: 0,
+      price: 15000,
+    },
+    {
+      id: "15",
+      image: "https://cdn-icons-png.flaticon.com/128/3345/3345397.png",
+      name: "Celana Pendek",
+      quantity: 0,
+      price: 10000,
+    },
+    {
+      id: "16",
+      image: "https://cdn-icons-png.flaticon.com/128/293/293241.png",
+      name: "Singlet",
+      quantity: 0,
+      price: 3000,
+    },
+  ];
+
+  return (
+    <>
+      <ScrollView style={{ backgroundColor: "#F0F0F0", flex: 1, marginTop: 50 }}>
+        {/* Location and Profile */}
+        <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+          <MaterialIcons name="location-on" size={30} color="#2491CB" />
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600" }}>Rumah</Text>
+            <Text>{displayCurrentAddress}</Text>
+          </View>
+
+          <Pressable onPress={() => navigation.navigate("Profile")} style={{ marginLeft: "auto", marginRight: 7 }}>
+            <Image
+              style={{ width: 40, height: 40, borderRadius: 20 }}
+              source={{
+                uri: "https://lh3.googleusercontent.com/ogw/AAEL6sh_yqHq38z35QMy5Fnb8ZIxicdxCIVM9PeBD2j-=s64-c-mo",
+              }}
+            />
+          </Pressable>
+        </View>
+
+        {/* Search Bar */}
+        <View
+          style={{
+            padding: 10,
+            margin: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            borderWidth: 0.8,
+            borderColor: "#C0C0C0",
+            borderRadius: 7,
+          }}
+        >
+          <TextInput placeholder="Cari item lain" />
+          <Feather name="search" size={24} color="#2491CB" />
+        </View>
+
+        {/* Image Carousel */}
+        <Carousel />
+
+        {/* Services Component */}
+        <Services />
+
+        {/* Render semua Produk */}
+        {product.map((item, index) => (
+          <DressItem item={item} key={index} />
+        ))}
+      </ScrollView>
+
+      {total === 0 ? null : (
+        <Pressable
+          style={{
+            backgroundColor: "#F468BB",
+            padding: 10,
+            marginBottom: 40,
+            margin: 15,
+            borderRadius: 7,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 15, fontWeight: "600", color: "white" }}>
+              {cart.length} items | Rp{total}
+            </Text>
+            <Text style={{ fontSize: 12, fontWeight: "400", color: "white", marginVertical: 6 }}>Biaya tambahan mungkin berlaku</Text>
+          </View>
+
+          <Pressable onPress={() => navigation.navigate("PickUp")}>
+            <Text style={{ fontSize: 15, fontWeight: "600", color: "white" }}>Lanjut ke pickup</Text>
+          </Pressable>
+        </Pressable>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
